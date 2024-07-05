8c0a9aeff46ec82b189decb1f132f4a67d957688
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-07-05 21:58:35 +0800

    no message

---
+++ Assets/Scripts/Framework/SingletonMono.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance()
+    {
+        return _instance;
+    }
+
+    protected virtual void Awake()
+    {
+        _instance = this as T;
+    }
+}
